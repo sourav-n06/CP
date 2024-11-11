9093b6def60129845e1c81dff2af25eddf299569
Sourav Hazra <souxhaz06@gmail.com>
2024-11-11 21:07:11 +0530

    Date-11nov24

---
+++ CodeForces/R981D3/SakurakoAndKosuke.cpp
@@ -0,0 +1,14 @@
+// #include <bits/stdc++.h>
+#include<iostream> 
+using namespace std;
+ 
+int main() {
+    int t = 0;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        if(n%2) cout<<"Kosuke"<<endl;
+        else cout<<"Sakurako"<<endl;
+    }
+}
